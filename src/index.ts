f03ccd3ba49192a30b1bff03d2dd9caa80d79f18
yuma-matsui <oshino.iiyo@gmail.com>
2022-07-10 12:39:37 +0900

    三項演算子に書き換え

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,9 @@ const rl = createInterface({
 rl.question('文字列を入力してください', (line) => {
   // 型推論でresult: numberとなる
   const result = Number(line) + 1000
-  if (Number.isNaN(result)) {
-    console.log('数値を入力してください')
-  } else {
-    console.log(`${result}が入力されました`)
-  }
+
+  Number.isNaN(result)
+  ? console.log('数値を入力してください')
+  : console.log(result)
   rl.close()
 })
