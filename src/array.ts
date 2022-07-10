2a81d0e98d11aeda274e3aa565098188e2998b42
yuma-matsui <oshino.iiyo@gmail.com>
2022-07-10 23:57:17 +0900

    力試し模範回答

--- src/array.ts
+++ src/array.ts
@@ -10,9 +10,23 @@ John Smith,17,0
 Mary Sue,14,1
 `
 
-const usersInfo = extractUserInfo(data)
-const users = translateUserInfo(usersInfo)
+const users: User[] = []
 
+const lines = data.trim().split('\n')
+for (const line of lines) {
+  const [name, ageString, premiumUserString] = line.split(',')
+  const age = Number(ageString)
+  const premiumUser = premiumUserString === '1'
+  users.push({
+    name,
+    age,
+    premiumUser
+  })
+}
+
+
+// const usersInfo = extractUserInfo(data)
+// const users = translateUserInfo(usersInfo)
 
 function extractUserInfo (data: string): string[][] {
   return data.trim().split('\n').map((user) => {
@@ -26,7 +40,7 @@ function translateUserInfo (usersData: string[][]): User[] {
     users.push({
       name,
       age: Number(age),
-      premiumUser: premiumUser === '1' ? true : false
+      premiumUser: premiumUser === '1'
     })
   }
   return users
