c7355498501ff2debbf069833055c747a085c343
yuma-matsui <oshino.iiyo@gmail.com>
2022-07-11 20:23:08 +0900

    力試し別解

--- src/array.ts
+++ src/array.ts
@@ -10,40 +10,19 @@ John Smith,17,0
 Mary Sue,14,1
 `
 
-const users: User[] = []
+const users: User[] = translateToUsersFromCSV(data)
 
-const lines = data.trim().split('\n')
-for (const line of lines) {
-  const [name, ageString, premiumUserString] = line.split(',')
-  const age = Number(ageString)
-  const premiumUser = premiumUserString === '1'
-  users.push({
-    name,
-    age,
-    premiumUser
-  })
-}
+function translateToUsersFromCSV (data: string): User[] {
+  return data.trim().split('\n')
+          .map(user => {
+            const [name, ageString, premiumUserString] = user.split(',')
 
-
-// const usersInfo = extractUserInfo(data)
-// const users = translateUserInfo(usersInfo)
-
-function extractUserInfo (data: string): string[][] {
-  return data.trim().split('\n').map((user) => {
-    return user.split(',')
-  })
-}
-
-function translateUserInfo (usersData: string[][]): User[] {
-  const users: User[] = []
-  for (const [name, age, premiumUser] of usersData) {
-    users.push({
-      name,
-      age: Number(age),
-      premiumUser: premiumUser === '1'
-    })
-  }
-  return users
+            return {
+              name,
+              age: Number(ageString),
+              premiumUser: premiumUserString === '1'
+            }
+          })
 }
 
 for (const user of users) {
