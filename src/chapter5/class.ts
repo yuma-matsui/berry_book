53ecd2e451f2dee62929870d778f85b83f31332c
yuma-matsui <oshino.iiyo@gmail.com>
2022-07-12 22:35:00 +0900

    ディレクトリの整理

---
+++ src/chapter5/class.ts
@@ -0,0 +1,19 @@
+type HasName = {
+  name: string;
+}
+
+
+class User implements HasName {
+  name: string
+  age: number
+
+  constructor (name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  isAdult(): boolean {
+    return this.age >= 20
+  }
+}
+
